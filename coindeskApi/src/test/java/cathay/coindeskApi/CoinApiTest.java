32ed9b3d619b3c251927c33be714e727f4dde34c
Drew Lee <drewlee@hitrust.tw>
2022-09-18 18:52:02 +0800

    [補] 補上CointApi測試（未完）

--- coindeskApi/src/test/java/cathay/coindeskApi/CoinApiTest.java
+++ coindeskApi/src/test/java/cathay/coindeskApi/CoinApiTest.java
@@ -1,17 +1,24 @@
 package cathay.coindeskApi;
 
 import static cathay.coindeskApi.util.JsonUtil.getJsonStringPrettyFormat;
+import static cathay.coindeskApi.util.JsonUtil.getJsonString;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import cathay.coindeskApi.api.vo.AddCoinRequest;
+import cathay.coindeskApi.api.vo.UpdateCoinRequest;
 
 @SpringBootTest
 @ExtendWith(SpringExtension.class)
@@ -22,10 +29,33 @@ public class CoinApiTest {
     private MockMvc mvc;
 	
 	@Test
+	@DisplayName("[List] Get all existing coin types")
 	public void test_list() throws Exception {
 		mvc.perform(
     		MockMvcRequestBuilders
-    		.get("/api/v1.0.0/coin/list"))
+    			.get("/api/v1.0.0/coin/list"))
+		 	.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+	
+	@Test
+	@DisplayName("[Add] New coinType")
+	public void test_add_new_coinType() throws Exception {
+		HttpHeaders httpHeaders = new HttpHeaders();
+	    httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+		
+	    AddCoinRequest request
+	    	= new AddCoinRequest()
+	            .setCoinCode("NTD")
+	            .setSymbol("NT$")
+	            .setRateFloat(1.0)
+	            .setDescription("New Taiwan Dollar")
+	            .setDescriptionChinese("新台幣");
+	    
+		mvc.perform(
+    		MockMvcRequestBuilders
+	    		.post("/api/v1.0.0/coin/add")
+	    		.headers(httpHeaders)
+	    		.content(getJsonString(request)))
 		 	.andExpect(MockMvcResultMatchers.status().isOk())
 		 	.andDo((MvcResult result) -> {
 		 		System.out.println("result: " + getJsonStringPrettyFormat(result.getResponse().getContentAsString()));
@@ -58,7 +88,19 @@ public class CoinApiTest {
 	}
 	
 	@Test
-	public void test_update_nonChanged_coinType_columns() {
+	@DisplayName("[Delete] Existing coinType")
+	public void test_delete_existing_coinTypes() throws Exception {
+		HttpHeaders httpHeaders = new HttpHeaders();
+	    httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
 		
+		mvc.perform(
+    		MockMvcRequestBuilders
+	    		.delete("/api/v1.0.0/coin/delete")
+	    		.headers(httpHeaders)
+	    		.param("code", "USD"))
+		 	.andExpect(MockMvcResultMatchers.status().isOk())
+		 	.andDo((MvcResult result) -> {
+		 		System.out.println("result: " + getJsonStringPrettyFormat(result.getResponse().getContentAsString()));
+		 	});	
 	}
 }
