5ed30c28c634e8074f3a1a7ce182dea1d5df0817
Drew Lee <drewlee@hitrust.tw>
2022-09-18 14:04:43 +0800

    [更正] data.sql：新增資料之註解

--- coindeskApi/src/main/resources/data.sql
+++ coindeskApi/src/main/resources/data.sql
@@ -1,4 +1,4 @@
--- 建立預設興趣
+-- 建立預設幣別
 INSERT INTO `coin_type` (code, symbol, rate, description, description_chinese, rate_float)
 VALUES ('USD', '&#36;', '20,176.4049', 'United State Dollar', '美金', 20176.4049),
        ('GBP', '&pound;', '16,859.2425', 'British Pound Sterling', '英鎊', 16859.2425),
